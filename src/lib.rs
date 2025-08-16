e831384b11719b039c82d53d3ec49ce376fd9d34
snqre <marco.bizxaro@gmail.com>
2025-08-16 15:21:01 +0200

    *

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 #![no_std]
 
-pub mod float;
-pub mod int;
 pub mod num;
 
 pub type Result<T> = ::core::result::Result<T, Error>;
--- src/num.rs
+++ src/num.rs
@@ -78,6 +78,146 @@ macro_rules! common_num_int_impl {
     };
 }
 
+macro_rules! common_int_impl_block_fragment {
+    () => {
+        fn checked_add(self, rhs: Self) -> Option<Self> {
+            self.checked_add(rhs)
+        }
+
+        fn checked_sub(self, rhs: Self) -> Option<Self> {
+            self.checked_sub(rhs)
+        }
+
+        fn checked_mul(self, rhs: Self) -> Option<Self> {
+            self.checked_mul(rhs)
+        }
+
+        fn checked_div(self, rhs: Self) -> Option<Self> {
+            self.checked_div(rhs)
+        }
+
+        fn checked_rem(self, rhs: Self) -> Option<Self> {
+            self.checked_rem(rhs)
+        }
+
+        fn checked_shl(self, rhs: u32) -> Option<Self> {
+            self.checked_shl(rhs)
+        }
+
+        fn checked_shr(self, rhs: u32) -> Option<Self> {
+            self.checked_shr(rhs)
+        }
+
+        fn checked_neg(self) -> Option<Self> {
+            self.checked_neg()
+        }
+
+        fn wrapping_add(self, rhs: Self) -> Self {
+            self.wrapping_add(rhs)
+        }
+
+        fn wrapping_sub(self, rhs: Self) -> Self {
+            self.wrapping_sub(rhs)
+        }
+
+        fn wrapping_mul(self, rhs: Self) -> Self {
+            self.wrapping_mul(rhs)
+        }
+
+        fn wrapping_div(self, rhs: Self) -> Self {
+            self.wrapping_div(rhs)
+        }
+
+        fn wrapping_rem(self, rhs: Self) -> Self {
+            self.wrapping_rem(rhs)
+        }
+
+        fn wrapping_pow(self, exponent: u32) -> Self {
+            self.wrapping_pow(exponent)
+        }
+
+        fn wrapping_neg(self) -> Self {
+            self.wrapping_neg()
+        }
+
+        fn overflowing_add(self, rhs: Self) -> (Self, bool) {
+            self.overflowing_add(rhs)
+        }
+
+        fn overflowing_sub(self, rhs: Self) -> (Self, bool) {
+            self.overflowing_sub(rhs)
+        }
+
+        fn overflowing_mul(self, rhs: Self) -> (Self, bool) {
+            self.overflowing_mul(rhs)
+        }
+
+        fn overflowing_div(self, rhs: Self) -> (Self, bool) {
+            self.overflowing_div(rhs)
+        }
+
+        fn overflowing_rem(self, rhs: Self) -> (Self, bool) {
+            self.overflowing_rem(rhs)
+        }
+
+        fn overflowing_shl(self, rhs: u32) -> (Self, bool) {
+            self.overflowing_shl(rhs)
+        }
+
+        fn overflowing_shr(self, rhs: u32) -> (Self, bool) {
+            self.overflowing_shr(rhs)
+        }
+
+        fn overflowing_neg(self) -> (Self, bool) {
+            self.overflowing_neg()
+        }
+
+        fn sqrt(self) -> Self {
+            self.isqrt()
+        }
+    };
+}
+
+macro_rules! common_signed_int_impl {
+    ($($ty:ident)*) => {
+        $(
+            impl Int for $ty {
+                const SIGNED: bool = true;
+                const BITS: Self = $ty::BITS as Self;
+                const BITS_U128: u128 = Self::BITS as u128;
+                const BITS_I128: i128 = Self::BITS as i128;
+                const MAX: Self = $ty::MAX as Self;
+                const MAX_U128: u128 = Self::MAX as u128;
+                const MAX_I128: i128 = Self::MAX as i128;
+                const MIN: Self = $ty::MIN as Self;
+                const MIN_U128: u128 = Self::MIN as u128;
+                const MIN_I128: i128 = Self::MIN as i128;
+                common_int_impl_block_fragment!();
+            }
+        )*
+    };
+}
+
+macro_rules! common_unsigned_int_impl {
+    ($($ty:ident)*) => {
+        $(
+            impl Int for $ty {
+                const SIGNED: bool = false;
+                const BITS: Self = $ty::BITS as Self;
+                const BITS_U128: u128 = Self::BITS as u128;
+                const BITS_I128: i128 = Self::BITS as i128;
+                const MAX: Self = $ty::MAX as Self;
+                const MAX_U128: u128 = Self::MAX as u128;
+                const MAX_I128: i128 = Self::MAX as i128;
+                const MIN: Self = $ty::MIN as Self;
+                const MIN_U128: u128 = Self::MIN as u128;
+                const MIN_I128: i128 = Self::MIN as i128;
+                common_int_impl_block_fragment!();
+            }
+        )*
+    };
+}
+
 pub trait ConstAs {
     common_const!(
         0 1 2 3 4 5 6 7 8 9
@@ -139,6 +279,87 @@ where
     Self: for<'a> ::core::ops::RemAssign<&'a Self>
 {}
 
+pub trait Int 
+where
+    Self: Num,
+    Self: ::core::cmp::Eq,
+    Self: ::core::cmp::Ord,
+    Self: ::core::convert::TryFrom<i8>,
+    Self: ::core::convert::TryFrom<i16>,
+    Self: ::core::convert::TryFrom<i32>,
+    Self: ::core::convert::TryFrom<i64>,
+    Self: ::core::convert::TryFrom<i128>,
+    Self: ::core::convert::TryFrom<isize>,
+    Self: ::core::convert::TryFrom<u8>,
+    Self: ::core::convert::TryFrom<u16>,
+    Self: ::core::convert::TryFrom<u32>,
+    Self: ::core::convert::TryFrom<u64>,
+    Self: ::core::convert::TryFrom<u128>,
+    Self: ::core::convert::TryFrom<usize>,
+    Self: ::core::convert::TryInto<i8>,
+    Self: ::core::convert::TryInto<i16>,
+    Self: ::core::convert::TryInto<i32>,
+    Self: ::core::convert::TryInto<i64>,
+    Self: ::core::convert::TryInto<i128>,
+    Self: ::core::convert::TryInto<isize>,
+    Self: ::core::convert::TryInto<u8>,
+    Self: ::core::convert::TryInto<u16>,
+    Self: ::core::convert::TryInto<u32>,
+    Self: ::core::convert::TryInto<u64>,
+    Self: ::core::convert::TryInto<u128>,
+    Self: ::core::convert::TryInto<usize>,
+    Self: ::core::ops::Shl<Output = Self>,
+    Self: ::core::ops::ShlAssign,
+    Self: ::core::ops::Shr<Output = Self>,
+    Self: ::core::ops::ShrAssign,
+    Self: ::core::ops::BitAnd<Output = Self>,
+    Self: ::core::ops::BitAndAssign,
+    Self: ::core::ops::BitOr<Output = Self>,
+    Self: ::core::ops::BitOrAssign,
+    Self: ::core::ops::BitXor<Output = Self>,
+    Self: ::core::ops::BitXorAssign {
+    const SIGNED: bool;
+    const BITS: Self;
+    const BITS_U128: u128;
+    const BITS_I128: i128;
+    const MAX: Self;
+    const MAX_U128: u128;
+    const MAX_I128: i128;
+    const MIN: Self;
+    const MIN_U128: u128 = 0;
+    const MIN_I128: i128 = 0;
+    fn checked_add(self, rhs: Self) -> Option<Self>;
+    fn checked_sub(self, rhs: Self) -> Option<Self>;
+    fn checked_mul(self, rhs: Self) -> Option<Self>;
+    fn checked_div(self, rhs: Self) -> Option<Self>;
+    fn checked_rem(self, rhs: Self) -> Option<Self>;
+    fn checked_shl(self, rhs: u32) -> Option<Self>;
+    fn checked_shr(self, rhs: u32) -> Option<Self>;
+    fn checked_neg(self) -> Option<Self>;
+    fn wrapping_add(self, rhs: Self) -> Self;
+    fn wrapping_sub(self, rhs: Self) -> Self;
+    fn wrapping_mul(self, rhs: Self) -> Self;
+    fn wrapping_div(self, rhs: Self) -> Self;
+    fn wrapping_rem(self, rhs: Self) -> Self;
+    fn wrapping_pow(self, exponent: u32) -> Self;
+    fn wrapping_neg(self) -> Self;
+    fn overflowing_add(self, rhs: Self) -> (Self, bool);
+    fn overflowing_sub(self, rhs: Self) -> (Self, bool);
+    fn overflowing_mul(self, rhs: Self) -> (Self, bool);
+    fn overflowing_div(self, rhs: Self) -> (Self, bool);
+    fn overflowing_rem(self, rhs: Self) -> (Self, bool);
+    fn overflowing_shl(self, rhs: u32) -> (Self, bool);
+    fn overflowing_shr(self, rhs: u32) -> (Self, bool);
+    fn overflowing_neg(self) -> (Self, bool);
+    fn sqrt(self) -> Self;
+}
+
+pub trait Float
+where
+    Self: Sized,
+    Self: Num
+{}
+
 common_num_float_impl!(
     f32
     f64
@@ -158,3 +379,24 @@ common_num_int_impl!(
     u128
     usize
 );
+
+common_signed_int_impl!(
+    i8
+    i16
+    i32
+    i64
+    i128
+    isize
+);
+
+common_unsigned_int_impl!(
+    u8
+    u16
+    u32
+    u64
+    u128
+    usize
+);
+
+impl Float for f32 {}
+impl Float for f64 {}
