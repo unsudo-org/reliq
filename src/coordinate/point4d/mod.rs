fb6b445954a6250b301ac07d77bc9bb57313ddbb
snqre <marco.bizxaro@gmail.com>
2025-08-24 10:23:59 +0200

    *

--- src/coordinate/point4d/mod.rs
+++ src/coordinate/point4d/mod.rs
@@ -1,37 +1,33 @@
 use super::*;
 
-::modwire::expose!(
-    pub alias
-);
-
 type Precision = u8;
 
 #[derive(Clone)]
-pub struct Point4D<const A: Precision, B, C, D = q::DefaultEngine>
+pub struct Point4D<const A: Precision, B, C = q::DefaultEngine>
 where
-    B: Int,
-    C: Timestamp,
-    D: q::Engine,
+    B: ops::Int,
+    B: ops::Prim,
+    C: q::Engine,
     (): q::SupportedPrecision<A>,
     (): q::SupportedInt<B>,
     (): q::Supported<A, B> {
-    pub timestamp: timestamp::Timestamp<C>,
-    pub x: q::Q<A, B, q::DefaultMode, D>,
-    pub y: q::Q<A, B, q::DefaultMode, D>,
-    pub z: q::Q<A, B, q::DefaultMode, D>
+    pub timestamp: ::core::time::Duration,
+    pub x: q::Q<A, B, q::DefaultMode, C>,
+    pub y: q::Q<A, B, q::DefaultMode, C>,
+    pub z: q::Q<A, B, q::DefaultMode, C>
 }
 
-impl<const A: Precision, B, C, D> Default for Point4D<A, B, C, D>
+impl<const A: Precision, B, C> Default for Point4D<A, B, C>
 where
-    B: Int,
-    C: Timestamp,
-    D: q::Engine,
+    B: ops::Int,
+    B: ops::Prim,
+    C: q::Engine,
     (): q::SupportedPrecision<A>,
     (): q::SupportedInt<B>,
     (): q::Supported<A, B> {
     fn default() -> Self {
         Self {
-            timestamp: C::AS_0.into(),
+            timestamp: ::core::time::Duration::default(),
             x: B::AS_0.into(),
             y: B::AS_0.into(),
             z: B::AS_0.into()
@@ -39,23 +35,22 @@ where
     }
 }
 
-impl<const A: Precision, B, C, D> From<(C, B, B, B)> for Point4D<A, B, C, D>
+impl<const A: Precision, B, C> From<(::core::time::Duration, B, B, B)> for Point4D<A, B, C>
 where
-    B: Int,
-    C: Timestamp,
-    D: q::Engine,
+    B: ops::Int,
+    B: ops::Prim,
+    C: q::Engine,
     (): q::SupportedPrecision<A>,
     (): q::SupportedInt<B>,
     (): q::Supported<A, B> {
-    fn from(value: (C, B, B, B)) -> Self {
-        let timestamp: C = value.0;
-        let timestamp: timestamp::Timestamp<_> = timestamp.into();
+    fn from(value: (::core::time::Duration, B, B, B)) -> Self {
+        let timestamp: ::core::time::Duration = value.0;
         let x: B = value.1;
-        let x: q::Q<A, B, q::DefaultMode, D> = x.into();
+        let x: q::Q<A, B, q::DefaultMode, C> = x.into();
         let y: B = value.2;
-        let y: q::Q<A, B, q::DefaultMode, D> = y.into();
+        let y: q::Q<A, B, q::DefaultMode, C> = y.into();
         let z: B = value.3;
-        let z: q::Q<A, B, q::DefaultMode, D> = z.into();
+        let z: q::Q<A, B, q::DefaultMode, C> = z.into();
         Self {
             timestamp,
             x,
@@ -65,24 +60,23 @@ where
     }
 }
 
-impl<const A: Precision, B, C, D> From<(B, B, B)> for Point4D<A, B, C, D>
+impl<const A: Precision, B, C> From<(B, B, B)> for Point4D<A, B, C>
 where
-    B: Int,
-    C: Timestamp,
-    D: q::Engine,
+    B: ops::Int,
+    B: ops::Prim,
+    C: q::Engine,
     (): q::SupportedPrecision<A>,
     (): q::SupportedInt<B>,
     (): q::Supported<A, B> {
     fn from(value: (B, B, B)) -> Self {
-        let timestamp: timestamp::Timestamp<C> = C::AS_0.into();
         let x: B = value.0;
-        let x: q::Q<A, B, q::DefaultMode, D> = x.into();
+        let x: q::Q<A, B, q::DefaultMode, C> = x.into();
         let y: B = value.1;
-        let y: q::Q<A, B, q::DefaultMode, D> = y.into();
+        let y: q::Q<A, B, q::DefaultMode, C> = y.into();
         let z: B = value.2;
-        let z: q::Q<A, B, q::DefaultMode, D> = z.into();
+        let z: q::Q<A, B, q::DefaultMode, C> = z.into();
         Self {
-            timestamp,
+            timestamp: ::core::time::Duration::default(),
             x,
             y,
             z
